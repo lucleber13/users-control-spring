91d48573075c384a672a757912947c9527ca1b70
lucleber13 <lucleber13@hotmail.com>
2022-07-19 00:23:50 +0100

    add methods to UserService interface

--- src/main/java/com/cbcode/car_locator/service/UserService.java
+++ src/main/java/com/cbcode/car_locator/service/UserService.java
@@ -4,6 +4,7 @@ import com.cbcode.car_locator.domain.User;
 import com.cbcode.car_locator.exception.domain.EmailExistException;
 import com.cbcode.car_locator.exception.domain.UserNotFoundException;
 import com.cbcode.car_locator.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -18,5 +19,15 @@ public interface UserService {
 
     User findUserByEmail(String email);
 
+    User addNewUser(String firstName, String lastName, String username, String email,
+                    String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
 
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail,
+                    String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(long id);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
