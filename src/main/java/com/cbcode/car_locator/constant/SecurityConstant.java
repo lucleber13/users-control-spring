94bee95a764f03287a33e9e79c493c85a8c8202e
lucleber13 <lucleber13@hotmail.com>
2022-07-19 13:41:34 +0100

    update EmailService and SecurityConstant

--- src/main/java/com/cbcode/car_locator/constant/SecurityConstant.java
+++ src/main/java/com/cbcode/car_locator/constant/SecurityConstant.java
@@ -16,7 +16,7 @@ public class SecurityConstant {
     public static final String JWT_TOKEN_HEADER = "Jwt-Token"; // custom header
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified"; // message response
     public static final String CB_CODE = "CB Code"; // company given access
-    public static final String GET_ARRAYS_ADMINISTRATION = "User Management Portal";
+    public static final String GET_CBCODE_ADMINISTRATION = "User Management Portal";
     public static final String AUTHORITIES = "authorities";
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
