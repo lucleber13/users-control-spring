5e70a24bf0c1cc77d6eb6d0882d69bcfa8cbe1e6
lucleber13 <lucleber13@hotmail.com>
2022-07-20 21:01:22 +0100

    add method UserResource

--- src/main/java/com/cbcode/car_locator/constant/FileConstant.java
+++ src/main/java/com/cbcode/car_locator/constant/FileConstant.java
@@ -9,6 +9,6 @@ public class FileConstant {
     public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
     public static final String DOT = ".";
     public static final String FORWARD_SLASH = "/";
-    public static final String NOT_AN_IMAGE_FILE = " is not an image file. Please upload an image file";
+//    public static final String NOT_AN_IMAGE_FILE = " is not an image file. Please upload an image file";
     public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
 }
--- src/main/java/com/cbcode/car_locator/resource/UserResource.java
+++ src/main/java/com/cbcode/car_locator/resource/UserResource.java
@@ -1,32 +1,46 @@
 package com.cbcode.car_locator.resource;
 
+import com.cbcode.car_locator.domain.HttpResponse;
 import com.cbcode.car_locator.domain.User;
 import com.cbcode.car_locator.domain.UserPrincipal;
 import com.cbcode.car_locator.exception.ExceptionHandling;
 import com.cbcode.car_locator.exception.domain.EmailExistException;
+import com.cbcode.car_locator.exception.domain.EmailNotFoundException;
 import com.cbcode.car_locator.exception.domain.UserNotFoundException;
 import com.cbcode.car_locator.exception.domain.UsernameExistException;
 import com.cbcode.car_locator.service.UserService;
 import com.cbcode.car_locator.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+import static com.cbcode.car_locator.constant.FileConstant.*;
 import static com.cbcode.car_locator.constant.SecurityConstant.JWT_TOKEN_HEADER;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserResource extends ExceptionHandling {
 
+    public static final String EMAIL_SENT = "An email with a new password was sent to: ";
+    public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully";
     private UserService userService;
     private AuthenticationManager authenticationManager;
     private JWTTokenProvider jwtTokenProvider;
@@ -48,14 +62,92 @@ public class UserResource extends ExceptionHandling {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException {
+    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException,
+            UsernameExistException, MessagingException {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<User> addNewUser(User user){
-        return null;
+    public ResponseEntity<User> addNewUser(@RequestParam ("firstName") String firstName,
+                                           @RequestParam ("lastName") String lastName,
+                                           @RequestParam ("username") String username,
+                                           @RequestParam ("email") String email,
+                                           @RequestParam ("role") String role,
+                                           @RequestParam ("isActive") String isActive,
+                                           @RequestParam ("isNonLocked") String isNonLocked,
+                                           @RequestParam (value = "profileImage", required = false) MultipartFile profileImage) throws UserNotFoundException, EmailExistException, IOException, UsernameExistException {
+        User newUser = userService.addNewUser(firstName, lastName, username, email, role, Boolean.parseBoolean(isNonLocked), Boolean.parseBoolean(isActive), profileImage);
+        return new ResponseEntity<>(newUser, OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<User> update(@RequestParam ("currentUsername") String currentUsername,
+                                       @RequestParam ("firstName") String firstName,
+                                       @RequestParam ("lastName") String lastName,
+                                       @RequestParam ("username") String username,
+                                       @RequestParam ("email") String email,
+                                       @RequestParam ("role") String role,
+                                       @RequestParam ("isActive") String isActive,
+                                       @RequestParam ("isNonLocked") String isNonLocked,
+                                       @RequestParam (value = "profileImage", required = false) MultipartFile profileImage) throws UserNotFoundException, EmailExistException, IOException, UsernameExistException {
+        User updateUser = userService.updateUser(currentUsername, firstName, lastName, username, email, role, Boolean.parseBoolean(isNonLocked), Boolean.parseBoolean(isActive), profileImage);
+        return new ResponseEntity<>(updateUser, OK);
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<User> getUser(@PathVariable("username") String username) {
+        User user = userService.findUserByUsername(username);
+        return new ResponseEntity<>(user, OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, OK);
+    }
+
+    @GetMapping("/resetPassword/{email}")
+    public ResponseEntity<HttpResponse> resetPassword(@PathVariable("email") String email) throws EmailNotFoundException, MessagingException {
+        userService.resetPassword(email);
+        return response(OK, EMAIL_SENT + email);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyAuthority('user:delete')")
+    public ResponseEntity<HttpResponse> deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return response(NO_CONTENT, USER_DELETED_SUCCESSFULLY);
+    }
+
+    @PostMapping("/updateProfileImage")
+    public ResponseEntity<User> updateProfileImage(@RequestParam ("username") String username, @RequestParam (value = "profileImage") MultipartFile profileImage) throws UserNotFoundException, EmailExistException, IOException, UsernameExistException {
+        User user = userService.updateProfileImage(username, profileImage);
+        return new ResponseEntity<>(user, OK);
+    }
+
+    @GetMapping(path = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE) // example add GIF = produces ={IMAGE_JPEG_VALUE, GIF}
+    public byte[] getProfileImage(@PathVariable("username") String username, @PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
+    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
+        return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(),
+                message.toUpperCase()), httpStatus);
     }
 
     private HttpHeaders getJwtHeader(UserPrincipal user) {
